0729ba54585124d0da0c8c43ffe5b2d0a2e8bf9a
Santiago Leon <sjleonh007@gmail.com>
2025-05-30 10:39:50 -0400

    mi primer commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// variables
+const bloque = document.getElementById('bloque');
+const boton = document.querySelector('button');
+const body = document.querySelector('body')
+
+// eventos
+bloque.addEventListener('mouseover', ()=>{
+    fondoBody();
+    
+});
+
+bloque.addEventListener('mouseout' , ()=>{
+     quitarFondo();
+})
+
+boton.addEventListener('click' , ()=>{
+    audio('audio/botonCompra.mp3');
+})
+
+
+// funciones
+function fondoBody(){
+    body.classList.add('cuerpo');
+
+}
+
+function quitarFondo(){
+    body.classList.remove('cuerpo');
+}
+
+function audio(url){
+    const audio = new Audio(url);
+    audio.play();
+}
